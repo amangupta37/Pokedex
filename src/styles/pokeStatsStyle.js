15959b0e675846c4171b73fc25de003a2f07f4af
Aman Gupta <amangupta6121998@gmail.com>
2021-12-12 15:18:27 +0530

    feat : Add Style for PokeStas Component

---
+++ src/styles/pokeStatsStyle.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  background: rgba(0, 0, 0, 0.8);
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: grid;
+  place-items: center;
+`;
