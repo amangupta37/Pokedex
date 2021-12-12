a5752f4f308221f0ed5829a6b327b7445533517a
Aman Gupta <amangupta6121998@gmail.com>
2021-12-12 20:27:34 +0530

    fix : Add Responsiveness for Popup  Component

--- src/styles/popupStyle.js
+++ src/styles/popupStyle.js
@@ -8,6 +8,25 @@ export const PopUpContainer = styled.section`
   box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
     rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
     rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+
+  @media only screen and (min-width: 0px) and (max-width: 380px) {
+    width: 100%;
+    height: 100vh;
+    overflow-y: scroll;
+    border-radius: 0rem;
+  }
+  @media only screen and (min-width: 381px) and (max-width: 600px) {
+    width: 100%;
+    height: 100vh;
+    overflow-y: scroll;
+    border-radius: 0rem;
+  }
+  @media only screen and (min-width: 601px) and (max-width: 900px) {
+    width: 100%;
+    height: 100vh;
+    overflow-y: scroll;
+    border-radius: 0rem;
+  }
 `;
 
 export const ClosePopUp = styled.section`
@@ -15,6 +34,19 @@ export const ClosePopUp = styled.section`
   height: 80px;
   cursor: pointer;
   padding: 1rem;
+
+  @media only screen and (min-width: 0px) and (max-width: 380px) {
+    width: 60px;
+    height: 60px;
+  }
+  @media only screen and (min-width: 381px) and (max-width: 600px) {
+    width: 60px;
+    height: 60px;
+  }
+  @media only screen and (min-width: 601px) and (max-width: 900px) {
+    width: 70px;
+    height: 70px;
+  }
 `;
 export const CloseIcon = styled.img`
   width: 100%;
@@ -23,7 +55,6 @@ export const CloseIcon = styled.img`
 
 export const StatsContainer = styled.section`
   width: 100%;
-  ${"" /* background: red; */}
 `;
 export const Title = styled.h1`
   text-align: center;
@@ -36,6 +67,19 @@ export const PokemonStats = styled.section`
   width: 100%;
   margin-top: 1rem;
   display: flex;
+  @media only screen and (min-width: 0px) and (max-width: 380px) {
+    flex-direction: column;
+    flex-direction: column-reverse;
+  }
+
+  @media only screen and (min-width: 381px) and (max-width: 600px) {
+    flex-direction: column;
+    flex-direction: column-reverse;
+  }
+  @media only screen and (min-width: 601px) and (max-width: 900px) {
+    flex-direction: column;
+    flex-direction: column-reverse;
+  }
 `;
 
 export const PokemonStatsCont = styled.section`
@@ -44,22 +88,50 @@ export const PokemonStatsCont = styled.section`
 
 export const PokemonModel = styled.section`
   width: 100%;
-  ${"" /* background: yellow; */}
+
   display: flex;
   padding: 1rem;
   gap: 1rem;
+  @media only screen and (min-width: 0px) and (max-width: 380px) {
+    flex-direction: column;
+  }
+
+  @media only screen and (min-width: 381px) and (max-width: 600px) {
+    flex-direction: column;
+  }
+  @media only screen and (min-width: 601px) and (max-width: 900px) {
+    flex-direction: column;
+  }
 `;
 export const PokemonImage = styled.section`
   width: 50%;
   height: 300px;
-  ${"" /* background: green; */}
+
   padding: 1rem;
   padding-right: 2rem;
+
+  @media only screen and (min-width: 0px) and (max-width: 380px) {
+    padding: 0rem;
+    padding-right: 0rem;
+    margin: 0 auto;
+  }
+
+  @media only screen and (min-width: 381px) and (max-width: 600px) {
+    padding: 0rem;
+    padding-right: 0rem;
+    margin: 0 auto;
+  }
+
+  @media only screen and (min-width: 601px) and (max-width: 900px) {
+    padding: 0rem;
+    padding-right: 0rem;
+    margin: 0 auto;
+  }
 `;
 export const PokemonStrength = styled.section`
   width: 70%;
   height: 30px;
-  ${"" /* background: blue; */}
+
   text-align: left;
   font-size: 1.1rem;
   font-weight: 600;
