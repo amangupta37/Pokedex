8faa8b161804c40e45a1697a7748b210e57727ef
Aman Gupta <amangupta6121998@gmail.com>
2021-12-12 11:42:05 +0530

    feat : upadate Home and Header Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../layout/Header";
+import { PokecardList } from "../layout/PokecardList";
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <PokecardList />
     </>
   );
 };
