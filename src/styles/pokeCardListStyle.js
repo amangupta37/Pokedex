ff3212cdab42d3c8a744357b2a509b2326fa6f61
Aman Gupta <amangupta6121998@gmail.com>
2021-12-12 20:26:36 +0530

    fix : Add Responsiveness for PokeCardList Component

--- src/styles/pokeCardListStyle.js
+++ src/styles/pokeCardListStyle.js
@@ -9,4 +9,34 @@ export const CardListContainer = styled.section`
   grid-template-columns: repeat(3, 1fr);
   grid-row-gap: 5rem;
   grid-column-gap: 2rem;
+
+  @media only screen and (min-width: 0px) and (max-width: 480px) {
+    grid-template-columns: repeat(1, 1fr);
+    grid-row-gap: 2rem;
+    grid-column-gap: 2rem;
+    margin-top: 2rem;
+    padding: 0;
+  }
+  @media only screen and (min-width: 390px) and (max-width: 610px) {
+    grid-template-columns: repeat(1, 1fr);
+    grid-row-gap: 2rem;
+    grid-column-gap: 2rem;
+    margin-top: 2rem;
+    padding: 0;
+  }
+
+  @media only screen and (min-width: 611px) and (max-width: 900px) {
+    grid-template-columns: repeat(1, 1fr);
+    grid-row-gap: 2rem;
+    grid-column-gap: 2rem;
+    margin-top: 2rem;
+    padding: 0;
+  }
+  @media only screen and (min-width: 901px) and (max-width: 1280px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-row-gap: 2rem;
+    grid-column-gap: 2rem;
+    margin-top: 2rem;
+    padding: 0;
+  }
 `;
