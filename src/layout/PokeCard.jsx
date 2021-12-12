21c91e51c05e5616f0eadb5ce31e4c8dedd0e36a
Aman Gupta <amangupta6121998@gmail.com>
2021-12-12 15:16:27 +0530

    feat : Add new stats

--- src/layout/PokeCard.jsx
+++ src/layout/PokeCard.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-expressions */
-import React from "react";
+import React, { useState } from "react";
 import {
   CardContainer,
   Top,
@@ -12,29 +12,40 @@ import {
   PokemonType,
   ViewStats,
 } from "../styles/pokeCardStyle.js";
+import { PokeStats } from "./PokeStats.jsx";
 
 export const PokeCard = ({ pokemonDetails }) => {
-  console.log(pokemonDetails.types[0].type.name);
+  const [showPokeStatsPoup, setShowPokeStatsPoup] = useState(false);
+  const showPokeStats = () => {
+    setShowPokeStatsPoup(true);
+  };
 
   return (
-    <CardContainer>
-      <Top>
-        <PokeName>
-          <Id>#{pokemonDetails.id}</Id>
-
-          <Name> {pokemonDetails.name}</Name>
-        </PokeName>
-        <PokeImage>
-          <Image
-            src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemonDetails.id}.svg`}
-            alt="mc"
-          />
-        </PokeImage>
-      </Top>
-      <Bottom>
-        <PokemonType>{pokemonDetails.types[0].type.name}</PokemonType>
-        <ViewStats>View Stats</ViewStats>
-      </Bottom>
-    </CardContainer>
+    <>
+      <CardContainer>
+        <Top>
+          <PokeName>
+            <Id>#{pokemonDetails.id}</Id>
+            <Name> {pokemonDetails.name}</Name>
+          </PokeName>
+          <PokeImage>
+            <Image
+              src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemonDetails.id}.svg`}
+              alt="pokemon_Image"
+            />
+          </PokeImage>
+        </Top>
+        <Bottom>
+          <PokemonType>{pokemonDetails.types[0].type.name}</PokemonType>
+          <ViewStats onClick={showPokeStats}>View Stats</ViewStats>
+        </Bottom>
+      </CardContainer>
+      {showPokeStatsPoup === true ? (
+        <PokeStats
+          managePopup={setShowPokeStatsPoup}
+          pokemonStats={pokemonDetails}
+        />
+      ) : null}
+    </>
   );
 };
